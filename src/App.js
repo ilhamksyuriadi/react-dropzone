50a7228e366ac5417713427da813bc7869945dae
ilham <ilhamksyuriadi@gmail.com>
2023-01-09 10:43:55 +0700

    chore: initialize and basic dropdown

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import 'react-dropzone-uploader/dist/styles.css'
+import Dropzone from 'react-dropzone-uploader'
+
+
+function App() {
+  // specify upload params and url for your files
+  const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
+  
+  // called every time a file's `status` changes
+  const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
+  
+  // receives array of files that are done uploading when submit button is clicked
+  const handleSubmit = (files, allFiles) => {
+    console.log(files.map(f => f.meta))
+    allFiles.forEach(f => f.remove())
+  }
+
+  return (
+    <div className="App">
+      <h1>File Uploader</h1>
+      <Dropzone
+        getUploadParams={getUploadParams}
+        onChangeStatus={handleChangeStatus}
+        onSubmit={handleSubmit}
+        accept="image/*,audio/*,video/*"
+      />
+    </div>
+  );
+}
+
+export default App;
